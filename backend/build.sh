bc5bf5d5e5f3b077a710ac6d3bdd55388f57732b
mukunzijonathan <mukunzijonathan010@gmail.com>
2026-02-24 13:07:52 +0200

    Added sthm in manage.py shell

--- backend/build.sh
+++ backend/build.sh
@@ -3,3 +3,14 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+python manage.py shell -c "
+from apps.accounts.models import User
+if not User.objects.filter(username='admin').exists():
+    u = User.objects.create_superuser('admin', 'admin@medpredict.com', 'Admin@2026')
+    u.role = 'admin'
+    u.save()
+    print('Admin created!')
+else:
+    print('Admin already exists')
+"
